3ef104a03bc49f61aa9d6b1fc4b6307c751a6d9b
eliasd1 <eliasdadoush23@gmail.com>
2021-04-15 14:54:36 +0300

    added a stack and queue class

---
+++ code-challenges/stacksandqueues/lib/src/main/java/Stack.java
@@ -0,0 +1,45 @@
+public class Stack<T>{
+  private Node top = null;
+  private Node current = null;
+
+  public void push(T object){
+    if(this.isEmpty()){
+      top = new Node(object);
+      current = new Node(object);
+    } else{
+      current = top;
+      top = new Node(object);
+      top.setNext(current);
+    }
+  }
+  public Node pop(){
+    if(this.isEmpty()){
+      throw new NullPointerException("Stack is empty");
+    } else{
+      Node temp = top;
+      top = top.next();
+      temp.setNext(null);
+      return temp;
+    }
+  }
+  public Node peek(){
+    if(this.isEmpty()){
+      throw new NullPointerException("Stack is empty");
+    } else{
+      return top;
+    }
+  }
+  public Boolean isEmpty(){
+    if(top == null){
+      return true;
+    }
+    return false;
+  }
+  @Override
+  public String toString() {
+    return "Stack{" +
+      "top=" + top.getData() +
+      ", current=" + current.getData() +
+      '}';
+  }
+}
---
+++ code-challenges/stacksandqueues/lib/src/test/java/StackTest.java
@@ -0,0 +1,21 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class StackTest {
+  @Test public void testStack(){
+    Stack stack = new Stack();
+    Stack secondStack = new Stack();
+    stack.push(5);
+    stack.push(8);
+    stack.push(10);
+
+    System.out.println("Test if pop works");
+    assertEquals(10, stack.pop().getData());
+    System.out.println("Test if peek works");
+    assertEquals(8, stack.peek().getData());
+    System.out.println("Test if isEmpty function works");
+    assertFalse(stack.isEmpty());
+  }
+}
